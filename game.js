30d72e0444049748925cfdc5b62f16dadb9cc187
sajjad <sajjad.sisakhtpour@gmail.com>
2024-03-08 10:36:12 +0330

    added image to project

--- game.js
+++ game.js
@@ -1,7 +1,13 @@
 var cvs = document.getElementById("mycan");
 var ctx = cvs.getContext("2d");
+var sprite=new Image()
+sprite.src='img/sprite.png'
 
-function draw() {}
+
+function draw() {
+  ctx.fillStyle = "#70c5ce";
+  ctx.fillRect(0, 0, cvs.width, cvs.height);
+}
 
 function update() {}
 
