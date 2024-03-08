5e89f48ba571d7a4ce4ea98719019c7b3e1a387e
sajjad <sajjad.sisakhtpour@gmail.com>
2024-03-08 10:49:59 +0330

    added ground

--- game.js
+++ game.js
@@ -37,10 +37,44 @@ var bg = {
   },
 };
 
+var fg = {
+  sX: 276,
+  sY: 0,
+  w: 499 - bg.w,
+  h: 111,
+  x: 0,
+  y: cvs.height - 111,
+  draw: function () {
+    ctx.drawImage(
+      sprite,
+      this.sX,
+      this.sY,
+      this.w,
+      this.h,
+      this.x,
+      this.y,
+      this.w,
+      this.h
+    );
+    ctx.drawImage(
+      sprite,
+      this.sX,
+      this.sY,
+      this.w,
+      this.h,
+      this.w,
+      this.y,
+      this.w,
+      this.h
+    );
+  },
+};
+
 function draw() {
   ctx.fillStyle = "#70c5ce";
   ctx.fillRect(0, 0, cvs.width, cvs.height);
   bg.draw();
+  fg.draw();
 }
 
 function update() {}
