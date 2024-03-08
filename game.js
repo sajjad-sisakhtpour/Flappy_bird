5d0eed85161b77838fb95c140214d7746040d9d7
sajjad <sajjad.sisakhtpour@gmail.com>
2024-03-08 10:48:07 +0330

    added background

--- game.js
+++ game.js
@@ -1,12 +1,46 @@
 var cvs = document.getElementById("mycan");
 var ctx = cvs.getContext("2d");
-var sprite=new Image()
-sprite.src='img/sprite.png'
 
+var sprite = new Image();
+sprite.src = "img/sprite.png";
+
+var bg = {
+  sX: 0,
+  sY: 0,
+  w: 275,
+  h: 227,
+  x: 0,
+  y: cvs.height - 226,
+  draw: function () {
+    ctx.drawImage(
+      sprite,
+      this.sX,
+      this.sY,
+      this.w,
+      this.h,
+      this.x,
+      this.y,
+      this.w,
+      this.h
+    );
+    ctx.drawImage(
+      sprite,
+      this.sX,
+      this.sY,
+      this.w,
+      this.h,
+      this.w,
+      this.y,
+      this.w,
+      this.h
+    );
+  },
+};
 
 function draw() {
   ctx.fillStyle = "#70c5ce";
   ctx.fillRect(0, 0, cvs.width, cvs.height);
+  bg.draw();
 }
 
 function update() {}
